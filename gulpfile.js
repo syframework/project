6c7af5b7dde18e96e7b199ef906dcbe8f28665d4
Michael Chan <michael.chan@sellzone.fr>
2022-09-08 18:37:51 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,65 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
-// Compile & minify css
-function css () {
-	var sass = require('gulp-sass')(require('sass'));
-	var postcss = require('gulp-postcss');
-	var autoprefixer = require('autoprefixer');
+// Javascript files to concat into assets/js/app.js
+const jsFiles = [
+	'node_modules/jquery/dist/jquery.min.js',
+	'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+	'node_modules/@fortawesome/fontawesome-free/js/all.min.js',
+	'node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js',
+	'node_modules/autosize/dist/autosize.min.js',
+	'protected/js/**/*.js'
+];
 
+// Concat all js files into assets/js/app.js
+function jsConcat() {
+	return gulp.src(jsFiles)
+		.pipe(concat('app.js'))
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./assets/js'));
+}
+
+// Uglify assets/js/app.js
+function jsUglify() {
+	return gulp.src('assets/js/app.js')
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./assets/js'));
+}
+
+// Compile & minify protected/scss/app.scss into assets/css/app.css
+function cssCompile() {
 	return gulp.src('./protected/scss/app.scss')
-			.pipe(sourcemaps.init())
-			.pipe(sass({outputStyle: 'compressed'}))
-			.pipe(postcss([autoprefixer()]))
-			.pipe(sourcemaps.write('.'))
-			.pipe(gulp.dest('./assets/css'));
-};
-
-// Compile & minify js
-function js () {
-	var concat = require('gulp-concat');
-	var uglify = require('gulp-uglify');
-	return gulp.src([
-		'./node_modules/jquery/dist/jquery.min.js',
-		'./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-		'./node_modules/@fortawesome/fontawesome-free/js/all.min.js',
-		'./node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js',
-		'./node_modules/autosize/dist/autosize.min.js',
-		'./protected/js/**/*.js'
-	]).pipe(concat('app.js')).pipe(sourcemaps.init()).pipe(uglify()).pipe(sourcemaps.write('.')).pipe(gulp.dest('./assets/js'))
-};
+		.pipe(sourcemaps.init())
+		.pipe(sass.sync({outputStyle: 'compressed'}))
+		.pipe(postcss([autoprefixer()]))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./assets/css'));
+}
 
 // Watch
 function watch () {
 	// Watch scss files
-	gulp.watch('protected/scss/**/*.scss', css);
+	gulp.watch('protected/scss/**/*.scss', cssCompile);
 
-	// Watch js file
-	gulp.watch('protected/js/**/*.js', js);
-};
+	// Watch js files
+	gulp.watch(jsFiles, jsConcat);
+}
 
 // Public task
-exports.css   = css;
-exports.js    = js;
+exports.css   = cssCompile;
+exports.js    = jsConcat;
 exports.watch = watch;
 
+// Build task
+exports.build = gulp.parallel(cssCompile, gulp.series(jsConcat, jsUglify));
+
 // Default Task
-exports.default = gulp.series(css, js, watch);
+exports.default = gulp.series(cssCompile, jsConcat, watch);
