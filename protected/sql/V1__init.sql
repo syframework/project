efdf2db59adb3b86744976b6d4c2dfba98a891b1
Syone <syone7@gmail.com>
2023-07-25 22:58:57 +0200

    add R files for the database

--- protected/sql/V1__init.sql
+++ protected/sql/V1__init.sql
@@ -13,17 +13,6 @@ CREATE TABLE `t_page` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
--- Records of t_page
--- ----------------------------
-INSERT INTO `t_page` (id, title) VALUES ('about-us', 'About us');
-INSERT INTO `t_page` (id, title) VALUES ('home', 'Home');
-INSERT INTO `t_page` (id, title) VALUES ('privacy', 'Privacy policy');
-INSERT INTO `t_page` (id, title) VALUES ('use', 'Conditions of use');
-INSERT INTO `t_page` (id, title) VALUES ('user-account', 'My account');
-INSERT INTO `t_page` (id, title) VALUES ('user-connection', 'Sign In');
-INSERT INTO `t_page` (id, title) VALUES ('user-password', 'Choose a new password');
-
--- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 CREATE TABLE `t_user` (
@@ -68,15 +57,6 @@ CREATE TABLE `t_user_permission` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
--- Records of t_user_permission
--- ----------------------------
-INSERT INTO `t_user_permission` VALUES ('page-create', 'Create a new page');
-INSERT INTO `t_user_permission` VALUES ('page-delete', 'Delete a page');
-INSERT INTO `t_user_permission` VALUES ('page-code', 'Update page source code');
-INSERT INTO `t_user_permission` VALUES ('page-update', 'Update a page settings');
-INSERT INTO `t_user_permission` VALUES ('page-update-inline', 'Update a page with inline edition (need page-update)');
-
--- ----------------------------
 -- Table structure for t_user_role
 -- ----------------------------
 CREATE TABLE `t_user_role` (
@@ -84,14 +64,6 @@ CREATE TABLE `t_user_role` (
   `description` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of t_user_role
--- ----------------------------
-INSERT INTO `t_user_role` VALUES ('admin', 'Administrator');
-INSERT INTO `t_user_role` VALUES ('blacklisted', 'Blacklisted user');
-INSERT INTO `t_user_role` VALUES ('super-admin', 'Super administrator');
-INSERT INTO `t_user_role` VALUES ('user', 'Simple user');
 
 -- ----------------------------
 -- Table structure for t_user_role_has_permission
@@ -105,15 +77,6 @@ CREATE TABLE `t_user_role_has_permission` (
   CONSTRAINT `t_user_role_has_permission_ibfk_1` FOREIGN KEY (`id_permission`) REFERENCES `t_user_permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `t_user_role_has_permission_ibfk_2` FOREIGN KEY (`id_role`) REFERENCES `t_user_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of t_user_role_has_permission
--- ----------------------------
-INSERT INTO `t_user_role_has_permission` VALUES ('super-admin', 'page-create');
-INSERT INTO `t_user_role_has_permission` VALUES ('super-admin', 'page-code');
-INSERT INTO `t_user_role_has_permission` VALUES ('super-admin', 'page-delete');
-INSERT INTO `t_user_role_has_permission` VALUES ('super-admin', 'page-update');
-INSERT INTO `t_user_role_has_permission` VALUES ('super-admin', 'page-update-inline');
 
 -- ----------------------------
 -- Table structure for t_user_setting
