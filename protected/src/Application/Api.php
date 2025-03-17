757bd34577c553d67292d4217482723c654ef5dd
Syone <syone7@gmail.com>
2025-03-17 18:10:00 +0100

    Remove dispatch method

--- protected/src/Application/Api.php
+++ protected/src/Application/Api.php
@@ -5,6 +5,11 @@ use Sy\Bootstrap\Lib\Str;
 
 class Api extends \Sy\Bootstrap\Application\Api {
 
+	/**
+	 * Add "foo/bar" to this white list if you want to remove security check for endpoint "/api/foo/bar"
+	 *
+	 * @var array
+	 */
 	private $whiteList = [];
 
 	public function security() {
@@ -15,15 +20,12 @@ class Api extends \Sy\Bootstrap\Application\Api {
 		return parent::security();
 	}
 
-	public function dispatch() {
-		// Check if a plugin api class exists
-		$class = 'Project\\Application\\Api\\' . ucfirst(Str::snakeToCaml($this->action));
-		if (class_exists($class)) {
-			$this->setVar('RESPONSE', new $class());
-			return;
-		}
-
-		parent::dispatch();
-	}
+	/**
+	 * Example: create a new endpoint "/api/foo"
+	 *
+	 * public function fooAction() {
+	 *     $this->ok();
+	 * }
+	 */
 
 }
